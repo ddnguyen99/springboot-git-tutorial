0a2a54581e12bded9c4f9ef95edf455fc49f4cbc
Dac Nguyen <dacnguyen2101@gmail.com>
2024-07-16 22:31:59 +0700

    Personal Practice: First Update REST API Please enter the commit message for your changes. Lines starting

---
+++ book-rest/src/test/java/com/ddnguyen/bookrest/BookRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddnguyen.bookrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
